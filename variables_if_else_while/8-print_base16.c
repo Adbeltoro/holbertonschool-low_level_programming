f1a02ef3312ce37b8659ab73f95d9567e3848de5
Holberton <adbeltoroasencio1@icloud.com>
2025-02-21 09:26:46 -0400

    task_8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+  char hex= '0';
+
+  while (hex <= '9')
+    {
+      putchar(hex);
+      hex++;
+
+    }
+  hex = 'a';
+  while (hex <= 'f')
+    {
+      putchar(hex);
+	hex++;
+    }
+  putchar('\n');
+
+  return (0);
+}
